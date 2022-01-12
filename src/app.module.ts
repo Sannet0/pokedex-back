498f099721ec4f832d572b1fa09ffaa68e78d37d
Sannet0 <shupletsov.exceedteam@gmail.com>
2022-01-12 16:53:04 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,33 +6,9 @@ import { PokemonsModule } from './pokemons/pokemons.module';
 import { UserModule } from './user/user.module';
 import { TokensModule } from './tokens/tokens.module';
 import { FavoritesModule } from './favorites/favorites.module';
-import { User } from './entity/user.entity';
-import { Favorites } from './entity/favorites.entity';
 
 @Module({
   imports: [
-    // TypeOrmModule.forRoot({
-    //   type: 'postgres',
-    //   host: 'database-1.cdfqr522jpvs.eu-west-2.rds.amazonaws.com',
-    //   port: 5432,
-    //   username: 'postgres',
-    //   password: 'postgres',
-    //   database: 'postgres',
-    //   synchronize: false,
-    //   logging: true,
-    //   entities: [
-    //     User,
-    //     Favorites
-    //   ],
-    //   migrations: [
-    //     __dirname + "/src/migration/*.ts"
-    //   ],
-    //   cli: {
-    //     "entitiesDir": __dirname + "/src/entity",
-    //     "migrationsDir": __dirname + "/src/migration"
-    //   },
-    //   autoLoadEntities: true,
-    // }),
     TypeOrmModule.forRoot(),
     PokemonsModule,
     UserModule,
