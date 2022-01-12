4d95ac0ba245b65b7d8e767008f76b97bcf6e19c
Sannet0 <shupletsov.exceedteam@gmail.com>
2022-01-12 16:54:23 +0300

    fix

--- src/main.ts
+++ src/main.ts
@@ -14,6 +14,6 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
   app.enableCors();
-  await app.listen(process.env.pors || 4000);
+  await app.listen(process.env.port || 4000);
 }
 bootstrap();
