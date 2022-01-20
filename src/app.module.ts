1e7375422cde8ca48c6b2dafb73efa2ed52e2b4a
Sannet0 <shupletsov.exceedteam@gmail.com>
2022-01-20 16:34:36 +0300

    fix

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,14 +8,26 @@ import { TokensModule } from './tokens/tokens.module';
 import { FavoritesModule } from './favorites/favorites.module';
 import { EmptyModule } from './empty/empty.module';
 import { BucketModule } from './bucket/bucket.module';
-import { DatabaseModule } from './database/database.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       envFilePath: ['.env'],
     }),
-    DatabaseModule,
+    TypeOrmModule.forRoot({
+      type: "postgres",
+      host: process.env.DB_HOST,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      port: Number(process.env.DB_PORT),
+      synchronize: true,
+      logging: false,
+      entities: [
+        "dist/**/*.entity{.ts,.js}"
+      ],
+      autoLoadEntities: true
+    }),
     PokemonsModule,
     UserModule,
     TokensModule,
